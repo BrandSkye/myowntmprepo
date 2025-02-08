3d730e1c36e92f8cf51f34cac0c84dd5a249c3a3
Dennis <dendinescode@gmail.com>
2025-02-08 11:29:45 +0100

    new url for scenario creator

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -18,7 +18,7 @@ const router = createRouter({
   routes: [
     { path: "/", component: Home }, //route to main page
     {
-      path: "/scenario",
+      path: "https://myowntmprepo.onrender.com/scenario",
       component: ScenarioCreator, //route to password protected scenario creator
       beforeEnter: (to, from, next) => {
         const userPassword = prompt("Enter the password:");
